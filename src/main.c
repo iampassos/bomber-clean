9c7ed9d93999aa9d668ffe52668c32e0fd332587
iampassos <78429887+iampassos@users.noreply.github.com>
2025-10-27 12:03:11 -0300

    Extending map draw logic

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <raylib.h>
 #include <stdlib.h>
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
 
 #define TILE_SIZE 48
-#define GRID_WIDTH 13
-#define GRID_HEIGHT 15
+#define GRID_WIDTH 15
+#define GRID_HEIGHT 13
+#define GRID_SIZE (GRID_WIDTH * GRID_HEIGHT)
 
 #define MAP_X_OFFSET (SCREEN_WIDTH / 2 - TILE_SIZE * GRID_WIDTH / 2)
-#define MAP_Y_OFFSET 0
+#define MAP_Y_OFFSET (SCREEN_HEIGHT / 2 - TILE_SIZE * GRID_HEIGHT / 2)
 
 typedef enum {
     TILE_EMPTY,
@@ -17,16 +18,26 @@ typedef enum {
     TILE_BRICK,
 } TileType;
 
+const Color TILE_COLORS[3] = {
+    [TILE_EMPTY] = (Color) {17, 92, 51, 255},
+    [TILE_WALL]  = (Color) {101, 101, 101, 255},
+    [TILE_BRICK] = (Color) {58, 58, 58, 255},
+};
+
 typedef struct Map {
-    TileType grid[GRID_WIDTH][GRID_HEIGHT];
+    TileType grid[GRID_HEIGHT][GRID_WIDTH];
 } Map;
 
 Map *init_map() {
     Map *map = malloc(sizeof(Map));
 
-    for (int i = 0; i < GRID_WIDTH; i++) {
+    for (int i = 0; i < GRID_HEIGHT; i++) {
         for (int j = 0; j < GRID_WIDTH; j++) {
-            map->grid[i][j] = TILE_EMPTY;
+            if (i == 0 || j == 0 || i + 1 == GRID_HEIGHT || j + 1 == GRID_WIDTH ||
+                (i % 2 == 0 && j % 2 == 0))
+                map->grid[i][j] = TILE_WALL;
+            else
+                map->grid[i][j] = TILE_EMPTY;
         }
     }
 
@@ -49,13 +60,19 @@ int main(void) {
 
         ClearBackground(RAYWHITE);
 
+        for (int i = 0; i < GRID_HEIGHT; i++)
+            for (int j = 0; j < GRID_WIDTH; j++)
+                DrawRectangle(MAP_X_OFFSET + j * TILE_SIZE, MAP_Y_OFFSET + i * TILE_SIZE, TILE_SIZE,
+                              TILE_SIZE, TILE_COLORS[map->grid[i][j]]);
+
         for (int i = 0; i <= GRID_WIDTH; i++)
             DrawLine(MAP_X_OFFSET + i * TILE_SIZE, MAP_Y_OFFSET, MAP_X_OFFSET + i * TILE_SIZE,
-                     MAP_Y_OFFSET + GRID_HEIGHT * TILE_SIZE, GRAY);
+                     MAP_Y_OFFSET + GRID_HEIGHT * TILE_SIZE, (Color) {128, 128, 128, 32});
 
         for (int i = 0; i < GRID_HEIGHT; i++)
             DrawLine(MAP_X_OFFSET, MAP_Y_OFFSET + i * TILE_SIZE,
-                     MAP_X_OFFSET + TILE_SIZE * GRID_WIDTH, MAP_Y_OFFSET + i * TILE_SIZE, GRAY);
+                     MAP_X_OFFSET + TILE_SIZE * GRID_WIDTH, MAP_Y_OFFSET + i * TILE_SIZE,
+                     (Color) {128, 128, 128, 32});
 
         EndDrawing();
     }
