4192f48e71940517cd7f4fdfbd5ad91075dfb918
iampassos <78429887+iampassos@users.noreply.github.com>
2025-10-27 14:46:06 -0300

    Formatter

--- src/bomb.c
+++ src/bomb.c
@@ -1,20 +1,20 @@
+#include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <raylib.h>
 
-typedef struct Bombs{
-    struct Bombs *head;
-    struct Bombs *tail;
-    int lenght;
-}Bombs;
+typedef struct Bombs {
+  struct Bombs *head;
+  struct Bombs *tail;
+  int lenght;
+} Bombs;
 
-typedef struct Bomb{
-    Vector2 posi;
-    double spawnTime;
-}Bomb;
+typedef struct Bomb {
+  Vector2 posi;
+  double spawnTime;
+} Bomb;
 
-void createBombsList(Bombs *list){
-    list->head=NULL;
-    list->tail=NULL;
-    list->lenght=0;
+void createBombsList(Bombs *list) {
+  list->head = NULL;
+  list->tail = NULL;
+  list->lenght = 0;
 }
--- src/main.c
+++ src/main.c
@@ -13,71 +13,74 @@
 #define MAP_Y_OFFSET (SCREEN_HEIGHT / 2 - TILE_SIZE * GRID_HEIGHT / 2)
 
 typedef enum {
-    TILE_EMPTY,
-    TILE_WALL,
-    TILE_BRICK,
+  TILE_EMPTY,
+  TILE_WALL,
+  TILE_BRICK,
 } TileType;
 
 const Color TILE_COLORS[3] = {
-    [TILE_EMPTY] = (Color) {17, 92, 51, 255},
-    [TILE_WALL]  = (Color) {101, 101, 101, 255},
-    [TILE_BRICK] = (Color) {58, 58, 58, 255},
+    [TILE_EMPTY] = (Color){17, 92, 51, 255},
+    [TILE_WALL] = (Color){101, 101, 101, 255},
+    [TILE_BRICK] = (Color){58, 58, 58, 255},
 };
 
 typedef struct Map {
-    TileType grid[GRID_HEIGHT][GRID_WIDTH];
+  TileType grid[GRID_HEIGHT][GRID_WIDTH];
 } Map;
 
 Map *init_map() {
-    Map *map = malloc(sizeof(Map));
+  Map *map = malloc(sizeof(Map));
 
-    for (int i = 0; i < GRID_HEIGHT; i++) {
-        for (int j = 0; j < GRID_WIDTH; j++) {
-            if (i == 0 || j == 0 || i + 1 == GRID_HEIGHT || j + 1 == GRID_WIDTH ||
-                (i % 2 == 0 && j % 2 == 0))
-                map->grid[i][j] = TILE_WALL;
-            else
-                map->grid[i][j] = TILE_EMPTY;
-        }
+  for (int i = 0; i < GRID_HEIGHT; i++) {
+    for (int j = 0; j < GRID_WIDTH; j++) {
+      if (i == 0 || j == 0 || i + 1 == GRID_HEIGHT || j + 1 == GRID_WIDTH ||
+          (i % 2 == 0 && j % 2 == 0))
+        map->grid[i][j] = TILE_WALL;
+      else
+        map->grid[i][j] = TILE_EMPTY;
     }
+  }
 
-    return map;
+  return map;
 }
 
 float delta_time = 0;
 
 int main(void) {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bomber Clean");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bomber Clean");
 
-    SetTargetFPS(60);
+  SetTargetFPS(60);
 
-    Map *map = init_map();
+  Map *map = init_map();
 
-    while (!WindowShouldClose()) {
-        delta_time = GetFrameTime();
+  while (!WindowShouldClose()) {
+    delta_time = GetFrameTime();
 
-        BeginDrawing();
+    BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+    ClearBackground(RAYWHITE);
 
-        for (int i = 0; i < GRID_HEIGHT; i++)
-            for (int j = 0; j < GRID_WIDTH; j++)
-                DrawRectangle(MAP_X_OFFSET + j * TILE_SIZE, MAP_Y_OFFSET + i * TILE_SIZE, TILE_SIZE,
-                              TILE_SIZE, TILE_COLORS[map->grid[i][j]]);
+    for (int i = 0; i < GRID_HEIGHT; i++)
+      for (int j = 0; j < GRID_WIDTH; j++)
+        DrawRectangle(MAP_X_OFFSET + j * TILE_SIZE,
+                      MAP_Y_OFFSET + i * TILE_SIZE, TILE_SIZE, TILE_SIZE,
+                      TILE_COLORS[map->grid[i][j]]);
 
-        for (int i = 0; i <= GRID_WIDTH; i++)
-            DrawLine(MAP_X_OFFSET + i * TILE_SIZE, MAP_Y_OFFSET, MAP_X_OFFSET + i * TILE_SIZE,
-                     MAP_Y_OFFSET + GRID_HEIGHT * TILE_SIZE, (Color) {128, 128, 128, 32});
+    for (int i = 0; i <= GRID_WIDTH; i++)
+      DrawLine(MAP_X_OFFSET + i * TILE_SIZE, MAP_Y_OFFSET,
+               MAP_X_OFFSET + i * TILE_SIZE,
+               MAP_Y_OFFSET + GRID_HEIGHT * TILE_SIZE,
+               (Color){128, 128, 128, 32});
 
-        for (int i = 0; i < GRID_HEIGHT; i++)
-            DrawLine(MAP_X_OFFSET, MAP_Y_OFFSET + i * TILE_SIZE,
-                     MAP_X_OFFSET + TILE_SIZE * GRID_WIDTH, MAP_Y_OFFSET + i * TILE_SIZE,
-                     (Color) {128, 128, 128, 32});
+    for (int i = 0; i < GRID_HEIGHT; i++)
+      DrawLine(MAP_X_OFFSET, MAP_Y_OFFSET + i * TILE_SIZE,
+               MAP_X_OFFSET + TILE_SIZE * GRID_WIDTH,
+               MAP_Y_OFFSET + i * TILE_SIZE, (Color){128, 128, 128, 32});
 
-        EndDrawing();
-    }
+    EndDrawing();
+  }
 
-    CloseWindow();
+  CloseWindow();
 
-    return 0;
+  return 0;
 }
