cb6bcdabcd67d86d9f0cdcf7bab2f6fc77545192
iampassos <78429887+iampassos@users.noreply.github.com>
2025-11-03 16:15:02 -0300

    Score

--- src/core/score.c
+++ src/core/score.c
@@ -1,34 +1,35 @@
-#include "score.h"
-#include "player.h"
-
-void score_get_player(Player *p1,Player *p2,Player *p3,Player *p4){
-    if(p1!=NULL){
-        //insert_score(p1->id,p1->death_time);
-    }
-}
-
-void insert_score(int id,double time){
-    //inserie no txt
-}
-
-char *score_get(){
-    
-}
-
-void swap_Score(Score *a,Score *b){
-    Score temp=*a;
-    *a=*b;
-    *b=temp;
-}
-
-void bubble_sort_score_by_time(Score *array, int len){
-    if(array !=NULL){
-        for(int i=1;i<len;i++){
-            int current=i;
-            while(current!=0 && array[current].time < array[current-1].time ){
-                swap_Score(&array[current],&array[current-1]);
-                current--;
-            }
-        }
-    }
-}
+// #include "score.h"
+// #include "player.h"
+//
+// void score_get_player(Player *p1,Player *p2,Player *p3,Player *p4){
+//     if(p1!=NULL){
+//         //insert_score(p1->id,p1->death_time);
+//     }
+// }
+//
+// void insert_score(int id,double time){
+//     //inserie no txt
+// }
+//
+// char *score_get(){
+//
+// }
+//
+// void swap_Score(Score *a,Score *b){
+//     Score temp=*a;
+//     *a=*b;
+//     *b=temp;
+// }
+//
+// void bubble_sort_score_by_time(Score *array, int len){
+//     if(array !=NULL){
+//         for(int i=1;i<len;i++){
+//             int current=i;
+//             while(current!=0 && array[current].time < array[current-1].time
+//             ){
+//                 swap_Score(&array[current],&array[current-1]);
+//                 current--;
+//             }
+//         }
+//     }
+// }
--- src/core/score.h
+++ src/core/score.h
@@ -1,14 +1,14 @@
 #ifndef SCORE_H
 #define SCORE_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct Score{
-    int id;
-    double time;
-}Score;
-
-void score_get_player(Player *p1,Player *p2,Player *p3,Player *p4);
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// typedef struct Score {
+//   int id;
+//   double time;
+// } Score;
+//
+// void score_get_player(Player *p1, Player *p2, Player *p3, Player *p4);
 
 #endif
