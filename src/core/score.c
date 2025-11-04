fb73a5a2d02451509f18dbddf04de824cc3f13ec
Thiago Fernandes de Medeiros <thfmadeiros01@gmail.com>
2025-11-04 11:27:15 -0300

    Refactor score functions and implement bubble sort

--- src/core/score.c
+++ src/core/score.c
@@ -1,53 +1,66 @@
-// #include "score.h"
-// #include "entities/player.h"
-// #include <stdio.h>
+#include "score.h"
 
-// void score_get_player(Player *p1){
-//     if(p1 != NULL){
-//         insert_score(p1->id, p1->time);
-//     } else {
-//         return;
-//     }
-// }
+ void swap_Score(Score *a, Score *b) {
+  Score temp = *a;
+  *a = *b;
+  *b = temp;
+}
 
-// void insert_score(int id, double time){
-//     FILE *file;
-//     file = fopen("Ranking.txt", "a");
-//     if (!file) return;
-//     fprintf(file, "Jogador %d Tempo: %lf\n", id, time);
-//     fclose(file);
-// }
+void bubble_sort_score_by_time(Score *array, int len) {
+  if (array != NULL) {
+    for (int i = 1; i < len; i++) {
+      int current = i;
+      while (current != 0 && array[current].time < array[current - 1].time) {
+        swap_Score(&array[current], &array[current - 1]);
+        current--;
+      }
+    }
+  }
+} 
 
-// char *score_get(){
-//     FILE *file;
-//     file = fopen("Ranking.txt", "r");
-//     if (!file) return;
-//     Player *array = malloc(sizeof(score));
-//     int i = 0;
-//     while (fscanf(file, "Jogador %d tempo%lf\n") != EOF)
-//     {
-//         fprintf("Jogador %d Tempo: %lf", array[i].id, array[i].time);
-//         i++;
-//     }
-//     fclose(file);
-//     bubble_sort_score_by_time(scores,count);
-    
-// }
+ void insert_score(int id, double time) {
+  FILE *file;
+  file = fopen("Ranking.txt", "a");
+  if (!file)
+    return;
+  fprintf(file, "Jogador %d Tempo: %lf\n", id, time);
+  fclose(file);
+}
 
-// void swap_Score(Score *a, Score *b){
-//     Score temp = *a;
-//     *a = *b;
-//     *b = temp;
-// }
+void score_get_player(Player *p1) {
+  if (p1 != NULL) {
+    insert_score(p1->id, 20);
+  } else {
+    return;
+  }
+}
 
-// void bubble_sort_score_by_time(Score *array, int len){
-//     if(array != NULL){
-//         for(int i = 1; i < len; i++){
-//             int current = i;
-//             while(current != 0 && array[current].time < array[current - 1].time){
-//                 swap_Score(&array[current], &array[current - 1]);
-//                 current--;
-//             }
-//         }
-//     }
-// }
+
+char *score_get() {
+  FILE *file;
+  file = fopen("Ranking.txt", "r");
+  if (!file)
+    return NULL;
+  Score *array = malloc(100 * sizeof(Score));
+  if (!array)
+    return NULL;
+  int cont = 0;
+  while (fscanf(file, "Jogador %d Tempo: %lf\n", &array[cont].id,
+                &array[cont].time) == 2) {
+    cont++;
+    if (cont >= 100)
+      break;
+  }
+  fclose(file);
+  bubble_sort_score_by_time(array, cont);
+  char *tabela = malloc(10000);
+  tabela[0] = '\0';
+  for (int i = 0; i < cont; i++) {
+    char linha[100];
+    sprintf(linha, "Jogador %d Tempo: %.2lf\n", array[i].id, array[i].time);
+    strcat(tabela, linha);
+  }
+
+  free(array);
+  return tabela;
+}
