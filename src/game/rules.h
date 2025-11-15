7942de2bc98120e5eac695299707e41c3457da38
Mircio <mfsn@cesar.school>
2025-11-14 22:44:30 -0300

    dococumentando rule

--- src/game/rules.h
+++ src/game/rules.h
@@ -6,10 +6,10 @@
 #include "entities/power_up.h"
 #include <stdbool.h>
 
-bool rules_can_end_game();
+bool rules_can_end_game(); // checa se todos os jogadores morreram para terminar o jogo
 
-bool rules_can_place_bomb(Player *player);
-bool rules_can_kill_player(Player *player);
+bool rules_can_place_bomb(Player *player); // checa se o jogador pode colocar uma bomba
+bool rules_can_kill_player(Player *player); // checa se o jogador esta invensivel
 bool rules_can_spawn_power_up(GridPosition grid);
 bool rules_can_kill_power_up(PowerUp *power_up);
 bool rules_can_kill_enemy(Enemy *enemy);
